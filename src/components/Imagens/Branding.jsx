e07527972b1bfad0a41618ba6a918ac93f723032
eCostaRibeiro <ecostaribeiro@outlook.com>
2023-01-17 23:29:39 -0300

    video tag update

--- src/components/Imagens/Branding.jsx
+++ src/components/Imagens/Branding.jsx
@@ -4,8 +4,8 @@ import Marca from "./logoMonn.svg"
 import bgVideo from "./bgVideo.mp4"
 export default props =>
     <div className="folder">
-        <video onloadstart="this.playbackRate = 0.0001;" id="bgVideo" loop autoPlay className="LogoBG">
-            <source src={bgVideo} />
+        <video onloadstart="this.playbackRate = 0.0001;" id="bgVideo" loop autoPlay muted className="LogoBG">
+            <source src={bgVideo} type="video/mp4" />
         </video>
         <img className="Branding"
             src={Marca}
